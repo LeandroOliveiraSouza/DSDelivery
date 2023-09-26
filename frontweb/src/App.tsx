e85babb361097ffdb65b2a4ba18fea33af820bb3
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-26 17:24:17 -0300

    Add Home page

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
