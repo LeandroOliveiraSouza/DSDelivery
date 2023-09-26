b4eaa56a9b53ef5475c2687e9e5e07bb2c688b49
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-26 17:08:17 -0300

    Add NavBar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <NavBar />
     </div>
   );
 }
