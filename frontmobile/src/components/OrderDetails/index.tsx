5282ae0e52649515a49b425b0d3d281b72b50ed0
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-10-01 16:42:01 -0300

    Implementing google maps url

--- frontmobile/src/components/OrderDetails/index.tsx
+++ frontmobile/src/components/OrderDetails/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, Linking, StyleSheet, Text, View } from "react-native";
 import Header from "../Header";
 import { Order } from "../../types";
 import { useNavigation } from "@react-navigation/native";
@@ -34,12 +34,18 @@ const OrderDetails = ({ route }: Props) => {
       );
   };
 
+  const handleStartNavigation = () => {
+    Linking.openURL(
+      `https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`
+    );
+  };
+
   return (
     <>
       <Header />
       <View style={styles.container}>
         <OrderCard order={order} />
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleStartNavigation}>
           <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
         </RectButton>
         <RectButton style={styles.button} onPress={handleConfirmDelivery}>
